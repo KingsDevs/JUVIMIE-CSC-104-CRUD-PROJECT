8948e2550a4e64b30d9d4df12e0a7d4799f11f4e
KingsDevs <sunnyckair@gmail.com>
2022-07-02 23:05:53 +0800

    added product class

--- src/main/java/com/juvimie/Product.java
+++ src/main/java/com/juvimie/Product.java
@@ -2,5 +2,46 @@ package com.juvimie;
 
 public class Product 
 {
-    
+    private int productId;
+    private String productName;
+    private Double productPrize;
+    private int quantity;
+    private String productType;
+    private String productOrigin;
+
+    public Product(int productId, String productName, Double productPrize, int quantity, String productType, String productOrigin)
+    {
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrize = productPrize;
+        this.quantity = quantity;
+        this.productType = productType;
+        this.productOrigin = productOrigin;
+    }
+
+    public String getProductName()
+    {
+        return productName;
+    }
+
+    public Double getProductPrize()
+    {
+        return productPrize;
+    }
+
+    public Integer getQuantity()
+    {
+        return quantity;
+    }
+
+    public String getProductType()
+    {
+        return productType;
+    }
+
+    public String getProductOrigin()
+    {
+        return productOrigin;
+    }
+
 }
