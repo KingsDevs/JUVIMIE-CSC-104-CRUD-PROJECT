fa7121d994c0f8ae05816715b580cc4483e027b1
KingsDevs <sunnyckair@gmail.com>
2022-07-04 23:54:47 +0800

    added alert coming soon!

--- src/main/java/com/juvimie/MainController.java
+++ src/main/java/com/juvimie/MainController.java
@@ -4,13 +4,17 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class MainController 
 {
     @FXML
     void buy(ActionEvent event) 
     {
-
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setContentText("Coming Soon!");
+        alert.show();
     }
 
     @FXML
